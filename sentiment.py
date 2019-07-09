d46092fee31c2b9a17f3ed47a530a6b26997446b
Brian <bdehlinger@luc.edu>
2019-07-09 16:02:25 -0700

    Requirements and file

---
+++ sentiment.py
@@ -0,0 +1,62 @@
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
+
+#import twitter keys and tokens
+from config import *
+
+# instance of elasticsearch is created as 
+elasticsearch = Elasticsearch()
+
+class TweetStreamListener(StreamListener):
+
+
+    def on_data(self, data):
+        
+        #decodes the json
+        tweet_dictionary = json.loads(data)
+        
+        #Retrieves tweet data into object tweet by using TextBlob
+        tweet = TextBlob(tweet_dictionary["text"])
+
+        #determine if sentiment is positive, negative, or netural
+
+        if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+
+        #output sentiment
+        print(sentiment)
+
+        elasticsearch.index(index="yosemite",
+                 doc_type="test-type",
+                 body={"author": tweet_dictionary["user"]["screen_name"],
+                       "date": tweet_dictionary["created_at"],
+                       "message": tweet_dictionary["text"],
+                       "polarity": tweet.sentiment.polarity,
+                       "subjectivity": tweet.sentiment.subjectivity,
+                       "sentiment": sentiment})
+        return True
+    
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    listener = TweetStreamListener()
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, listener)
+
+
+    stream.filter(track=['national park'])
